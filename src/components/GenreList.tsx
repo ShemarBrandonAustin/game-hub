e118728e27e2d3ab08d242f46ce9e5b3db4ee76c
Shemar <shemar@techlify.com>
2023-07-28 22:52:09 -0400

    Cleanup Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -27,28 +28,34 @@ const GenreList = ({ onSelect, selectedGenre }: Props) => {
   }
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY={'5px'}>
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              alt={genre.name}
-              boxSize={'32px'}
-              borderRadius={8}
-            />
-            <Button
-              fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal'}
-              fontSize={'lg'}
-              variant={'link'}
-              onClick={() => onSelect(genre)}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading as='h1' fontSize='2xl' marginBottom={3}> Genres </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY={'5px'}>
+            <HStack>
+              <Image
+                objectFit='cover'
+                src={getCroppedImageUrl(genre.image_background)}
+                alt={genre.name}
+                boxSize={'32px'}
+                borderRadius={8}
+              />
+              <Button
+                whiteSpace='normal'
+                textAlign='left'
+                fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal'}
+                fontSize={'lg'}
+                variant={'link'}
+                onClick={() => onSelect(genre)}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   )
 }
 
